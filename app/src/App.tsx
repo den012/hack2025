dffa20b30fa63e6ac98afa9ecb2803e48f400cc6
Denis <blagadeniss@gmail.com>
2025-12-06 14:14:51 +0200

    google login

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,31 +1,48 @@
 import React from 'react';
 import axios from 'axios';
 
-const App: React.FC = () => {
-  const API_URL = import.meta.env.VITE_API_URL;
-  const [response, setReponse ] = React.useState<string>('');
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(`${API_URL}/greet`, {
-        headers: {
-          "Content-Type": "application/json",
-          "ngrok-skip-browser-warning": "true"
-        }
-      });
-      setReponse(response.data);
-      console.log(response.data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  }
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import Home from './components/Home';
+import Login from './components/GoogleAuth/GoogleLogin';
+
+// const App: React.FC = () => {
+//   const API_URL = import.meta.env.VITE_API_URL;
+//   const [response, setReponse ] = React.useState<string>('');
+//   const fetchData = async () => {
+//     try {
+//       const response = await axios.get(`${API_URL}/greet`, {
+//         headers: {
+//           "Content-Type": "application/json",
+//           "ngrok-skip-browser-warning": "true"
+//         }
+//       });
+//       setReponse(response.data);
+//       console.log(response.data);
+//     } catch (error) {
+//       console.error('Error fetching data:', error);
+//     }
+//   }
+
+//   return (
+//     <div>
+//       <h1 className="text-blue-300">Hello, World!</h1>
+//       <button onClick={fetchData} className="px-4 py-2 bg-green-500 text-white rounded">Fetch Greeting</button>
+//       {response && <p className="mt-4 text-gray-700">{response}</p>}
+//     </div>
+//   )
+// } 
+
+
+const App: React.FC = () => {
   return (
-    <div>
-      <h1 className="text-blue-300">Hello, World!</h1>
-      <button onClick={fetchData} className="px-4 py-2 bg-green-500 text-white rounded">Fetch Greeting</button>
-      {response && <p className="mt-4 text-gray-700">{response}</p>}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
   )
-} 
+}
 
 export default App;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,53 @@
-import express, { Request, Response } from "express";
-import cors from "cors";
+// import express, { Request, Response } from "express";
+// import cors from "cors";
 
-import { config } from "./config/index";
+// import { config } from "./config/index";
+
+// const app = express();
+// app.use(cors());
+
+// const PORT = config.PORT
+
+// app.get("/greet", (req: Request, res: Response) => {
+//   res.send("Hello");
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server running at http://localhost:${PORT}`);
+// });
+
+
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import chalk from 'chalk';
+
+//database
+import db from './database/database';
+
+import userRoutes from './routes/authRoutes';
+
+//routes
+
+dotenv.config();
 
 const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
 app.use(cors());
 
-const PORT = config.PORT
+app.use(cookieParser());
 
-app.get("/greet", (req: Request, res: Response) => {
-  res.send("Hello");
+app.use('/api', userRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
+
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+    console.log('Server is running on http://localhost:'+ chalk.green(`${PORT}`));
+})
