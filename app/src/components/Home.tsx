2bc2130de307a67002b0fa7cf09ef19c5881f0e7
Denis <blagadeniss@gmail.com>
2025-12-06 15:53:19 +0200

    home changes

--- app/src/components/Home.tsx
+++ app/src/components/Home.tsx
@@ -1,12 +1,76 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { onAuthStateChanged, type User } from 'firebase/auth';
+import { auth } from './GoogleAuth/Config';
+import { useNavigate } from 'react-router-dom';
+
+import L from 'leaflet';
 
 const Home: React.FC = () => {
+    const [user, setUser] = useState<User | null>(null);
+    const navigate = useNavigate();
+
+    const mapContainer = React.useRef<HTMLDivElement | null>(null);
+    const mapInstance = React.useRef<L.Map | null>(null);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            if (currentUser) {
+                setUser(currentUser);
+            } else {
+                navigate('/');
+            }
+        });
+
+        return () => unsubscribe();
+    }, [navigate]);
+
+    useEffect(() => {
+        // Initialize map only when the container is available and the map isn't already initialized
+        if (mapContainer.current && !mapInstance.current) {
+            mapInstance.current = L.map(mapContainer.current).setView([51.505, -0.09], 13);
+
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(mapInstance.current);
+
+            L.marker([51.5, -0.09]).addTo(mapInstance.current)
+                .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+                .openPopup();
+        }
+
+        // Cleanup function to remove map on component unmount
+        return () => {
+            if (mapInstance.current) {
+                mapInstance.current.remove();
+                mapInstance.current = null;
+            }
+        };
+    }, [user]);
+
+    if (!user) {
+        return <div>Loading...</div>;
+    }
+
+
+
     return (
         <div>
             <h2>Home Page!</h2>
+            <p>Welcome, {user.displayName}!</p>
+            <p>Email: {user.email}</p>
+            {user.photoURL && (
+                <img 
+                    src={user.photoURL} 
+                    alt="Profile" 
+                    style={{ borderRadius: '50%', width: '50px', height: '50px' }} 
+                />
+            )}
+
+            <div ref={mapContainer} style={{ height: "400px", width: "100%" }} />
         </div>
-    )
+    );
 }
+
 
 
 export default Home;
