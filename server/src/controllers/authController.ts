b22c6fad7960402f3076e9022e4b72b6d102824c
Denis <blagadeniss@gmail.com>
2025-12-06 14:34:53 +0200

    fixed login w database

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,20 +1,24 @@
 import { Request, Response } from 'express';
 import db  from '../database/database';
+import { ResultSetHeader } from 'mysql2';
 
 export const saveUser = async (req: Request, res: Response) => {
     const {uid, name, email, photo} = req.body;
 
-    let userCount = 0;
-
     try {
-        await db.query(
-            'INSERT INTO users (id, name, email, photo) VALUES (?, ?, ?, ?)',
+        const [result] = await db.query<ResultSetHeader>(
+            'INSERT IGNORE INTO users (id, name, email, photo) VALUES (?, ?, ?, ?)',
             [uid, name, email, photo]
         );
-        userCount++;
-        console.log('User saved successfully');
-        console.log(`Total users saved: ${userCount}`);
-        res.status(201).json({ message: 'User saved successfully' });
+
+
+        if (result.affectedRows > 0) {
+            console.log('New user saved successfully:', name);
+            res.status(201).json({ message: 'User created successfully' });
+        } else {
+            console.log('User already exists:', name);
+            res.status(200).json({ message: 'User already exists' });
+        }
     } catch (error) {
         res.status(500).json({ message: 'Failed to save user', error });
     }
