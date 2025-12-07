4462130993ff2b25724fdd71ef3ce7b8363a1d81
Denis <blagadeniss@gmail.com>
2025-12-07 12:16:45 +0200

    changed vite config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -13,18 +13,60 @@ export default defineConfig({
   plugins: [
     react(), 
     tailwindcss(),
-      VitePWA({
+    VitePWA({
       registerType: 'autoUpdate',
-      srcDir: 'public',
-      filename: 'service-worker.js',
-      strategies: 'injectManifest',
-      
-      // We don't need to define the manifest here because 
-      // injectManifest will use your existing service worker logic.
-      // The plugin will automatically find and inject the list of assets to cache.
-      injectManifest: {
-        // This tells the plugin where to find the assets to add to the precache list.
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      includeAssets: ['vite.svg'],
+      manifest: {
+        name: 'BunkerFinder',
+        short_name: 'BunkerFinder',
+        description: 'Find the nearest bunker in case of emergency',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'vite.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml'
+          },
+          {
+            src: 'vite.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        cleanupOutdatedCaches: true,
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/tile\.openstreetmap\.org\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'map-tiles-cache',
+              expiration: {
+                maxEntries: 200,
+                maxAgeSeconds: 60 * 60 * 24 * 30 // 30 Days
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          },
+          {
+            urlPattern: ({ url }) => url.hostname.includes('ngrok-free.app'),
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'api-data-cache',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24 // 1 Day
+              },
+              cacheableResponse: {
+                statuses: [200]
+              }
+            }
+          }
+        ]
       }
     })
   ],
